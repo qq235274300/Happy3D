8b6c49b05acdd41dbc69570c6972d91d3d4c5629
Chill <shaolj2@lenovo.com>
2023-06-15 00:03:04 +0800

    完成Keyboard类

--- NewD3DEngine/Keyboard.cpp
+++ NewD3DEngine/Keyboard.cpp
@@ -1,4 +1,5 @@
 #include "Keyboard.h"
+#include <optional>
 
 bool Keyboard::IsKeyPressed(unsigned char keycode) const noexcept
 {
@@ -9,12 +10,103 @@ Keyboard::Event Keyboard::ReadKey() noexcept
 {
 	if (keyBuffer.size() > 0u)
 	{
-		Event e = keyBuffer.front();
+		Keyboard::Event e = keyBuffer.front();
 		keyBuffer.pop();
 		return e;
 	}
 	else
 	{
 		return Keyboard::Event();
+		//return std::optional<Keyboard::Event>();
 	}
 }
+
+bool Keyboard::KeyIsEmpty() const noexcept
+{
+	return keyBuffer.empty();
+}
+
+void Keyboard::FlushKey() noexcept
+{
+	keyBuffer = std::queue<Keyboard::Event>();
+}
+
+char Keyboard::ReadChar() noexcept
+{
+	if (CharIsEmpty())
+	{
+		return 0;
+	}
+	else
+	{
+		unsigned char target = charBuffer.front();
+		charBuffer.pop();
+		return target;
+	}
+}
+
+bool Keyboard::CharIsEmpty() const noexcept
+{
+	return charBuffer.empty();
+}
+
+void Keyboard::FlushChar() noexcept
+{
+	charBuffer = std::queue<char>();
+}
+
+void Keyboard::Flush() noexcept
+{
+	keyBuffer = std::queue<Keyboard::Event>();
+	charBuffer = std::queue<char>();
+}
+
+void Keyboard::EnableAutorepeat() noexcept
+{
+	autorepeatEnable = true;
+}
+
+void Keyboard::DisableAutorepeat() noexcept
+{
+	autorepeatEnable = false;
+}
+
+bool Keyboard::AutorepeatIsEnabled() const noexcept
+{
+	return autorepeatEnable;
+}
+
+void Keyboard::OnKeyPressed(unsigned char keycode) noexcept
+{
+	keyStates[keycode] = true;
+	keyBuffer.push(Keyboard::Event(Keyboard::Event::Type::Press, keycode));
+	TrimBuffer(keyBuffer);
+}
+
+void Keyboard::OnKeyReleased(unsigned char keycode) noexcept
+{
+	keyStates[keycode] = false;
+	keyBuffer.push(Keyboard::Event(Keyboard::Event::Type::Release, keycode));
+	TrimBuffer(keyBuffer);
+}
+
+void Keyboard::OnChar(char character) noexcept
+{
+	charBuffer.push(character);
+	TrimBuffer(charBuffer);
+}
+
+void Keyboard::ClearState() noexcept
+{
+	keyStates.reset();
+}
+
+template<typename T>
+void Keyboard::TrimBuffer(std::queue<T>& buffer) noexcept
+{
+	while (buffer.size() > nBufferSize)
+	{
+		buffer.pop();
+	}
+}
+
--- NewD3DEngine/Keyboard.h
+++ NewD3DEngine/Keyboard.h
@@ -1,4 +1,7 @@
 #pragma once
+
+#include "Window.h"
+
 #include <bitset>
 #include <queue>
 
@@ -6,6 +9,10 @@
 
 class Keyboard
 {
+
+	friend class Window;
+
+public:
 	class Event
 	{
 	public:
@@ -21,6 +28,12 @@ class Keyboard
 		unsigned char keycode;
 		
 	public:
+		Event()noexcept
+			:type(Type::Invalid), keycode(0u)
+		{
+			
+		}
+
 		Event(Type inType) noexcept
 			:type(inType), keycode(0u)
 		{
@@ -62,13 +75,41 @@ public:
 	//EVENT
 	bool IsKeyPressed(unsigned char keycode)const noexcept;
 	Event ReadKey()noexcept;
+	bool KeyIsEmpty()const noexcept;
+	void FlushKey()noexcept;
+
+	//Char Text Input
+	char ReadChar() noexcept;
+	bool CharIsEmpty()const noexcept;
+	void FlushChar()noexcept;
+	
+	void Flush()noexcept;
+	
+	//autoRepeat control
+	void EnableAutorepeat()noexcept;
+	void DisableAutorepeat()noexcept;
+	bool AutorepeatIsEnabled()const noexcept;
+
+private:
+	//This Functions only use in WinAPI Side
+	void OnKeyPressed(unsigned char keycode)noexcept;
+	void OnKeyReleased(unsigned char keycode)noexcept;
+	void OnChar(char character)noexcept;
+	void ClearState()noexcept;
+	template<typename T>
+	static void TrimBuffer(std::queue<T>& buffer) noexcept;
 
 private:
 	//
 	static constexpr unsigned int nKeys = 256u;
+	static constexpr unsigned int nBufferSize = 16u;
+	//keyStates 由于Windows KeyMessages 在一个Byte内表示 0-255 ，所以可以用位运算bitset 表示，true表示这个keycode 按下
 	std::bitset<nKeys> keyStates;
 	std::queue<Event> keyBuffer;
 	std::queue<char> charBuffer;
+
+	//
+	bool autorepeatEnable = false;
 	
 };
 
